00fdb7234c7832ee2d7c57d564a99472f6277acf
LianHirsh <48855150+LianHirsh@users.noreply.github.com>
2019-04-13 16:20:25 +0300

    scraper ingredient union

--- src/CookEat/Tools/scrapers/HashefHalavanScraper.cs
+++ src/CookEat/Tools/scrapers/HashefHalavanScraper.cs
@@ -39,48 +39,5 @@ namespace CookEat.Tools.scrapers
 
             return recipeToAdd;
         }
-
-        private List<Ingredient> createIngredientsList(HtmlNodeCollection ingredients)
-        {
-            List<double> IngredientsListAmounts = new List<double>();
-            List<string> IngredientsNamesList = new List<string>();
-
-            for (int currIngredient = 0; currIngredient < ingredients.Count; currIngredient++)
-            {
-                string nodeToString = ingredients[currIngredient].InnerText;
-                string[] WordsInStr = nodeToString.Split(' ');
-                double amount;
-                if (double.TryParse(WordsInStr[0], out amount))
-                {
-                    IngredientsListAmounts.Add(amount);
-                    string ingredientName = null;
-                    for (int wordToConcatIndex = 1; wordToConcatIndex < WordsInStr.Length; wordToConcatIndex++)
-                    {
-                        ingredientName += WordsInStr[wordToConcatIndex];
-                        ingredientName += ' ';
-                    }
-
-                    IngredientsNamesList.Add(ingredientName);
-
-                }
-                else
-                {
-                    IngredientsListAmounts.Add(0);
-                    IngredientsNamesList.Add(ingredients[currIngredient].InnerText);
-                }
-            }
-
-            List<Ingredient> ingredientsList = new List<Ingredient>();
-
-            for (int currIngredientIndex = 0; currIngredientIndex < IngredientsListAmounts.Count; currIngredientIndex++)
-            {
-                ingredientsList[currIngredientIndex].Quantity.Amount = IngredientsListAmounts[currIngredientIndex];
-                ingredientsList[currIngredientIndex].Name = IngredientsNamesList[currIngredientIndex];
-            }
-
-            return ingredientsList;
-        }
-
-
     }
 }
--- src/CookEat/Tools/scrapers/Scraper.cs
+++ src/CookEat/Tools/scrapers/Scraper.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
+using HtmlAgilityPack;
 
 namespace CookEat
 {
@@ -39,5 +40,58 @@ namespace CookEat
 
             return numOfDishes;
         }
-    }
+
+		protected List<Ingredient> createIngredientsList(HtmlNodeCollection ingredients)
+		{
+			List<double> IngredientsListAmounts = new List<double>();
+			List<string> IngredientsNamesList = new List<string>();
+
+			for (int currIngredient = 0; currIngredient < ingredients.Count; currIngredient++)
+			{
+				string nodeToString = ingredients[currIngredient].InnerText;
+				string[] WordsInStr = nodeToString.Split(' ');
+				double amount;
+				if (double.TryParse(WordsInStr[0], out amount))
+				{
+					IngredientsListAmounts.Add(amount);
+					IngredientsNamesList.Add(concatIngredientName(WordsInStr));
+
+				}
+				else if (Constants.IngredientNameToAmount.ContainsKey(WordsInStr[0]))
+				{
+					double amountValue = Constants.IngredientNameToAmount[WordsInStr[0]];
+
+					IngredientsListAmounts.Add(amountValue);
+					IngredientsNamesList.Add(concatIngredientName(WordsInStr));
+				}
+				else
+				{
+					IngredientsListAmounts.Add(0);
+					IngredientsNamesList.Add(ingredients[currIngredient].InnerText);
+				}
+			}
+
+			List<Ingredient> ingredientsList = new List<Ingredient>();
+
+			for (int currIngredientIndex = 0; currIngredientIndex < IngredientsListAmounts.Count; currIngredientIndex++)
+			{
+				ingredientsList[currIngredientIndex].Quantity.Amount = IngredientsListAmounts[currIngredientIndex];
+				ingredientsList[currIngredientIndex].Name = IngredientsNamesList[currIngredientIndex];
+			}
+
+			return ingredientsList;
+		}
+
+		private string concatIngredientName(string[] splitedWords)
+		{
+			string ingredientName = null;
+			for (int wordToConcatIndex = 1; wordToConcatIndex < splitedWords.Length; wordToConcatIndex++)
+			{
+				ingredientName += splitedWords[wordToConcatIndex];
+				ingredientName += ' ';
+			}
+
+			return ingredientName;
+		}
+	}
 }
