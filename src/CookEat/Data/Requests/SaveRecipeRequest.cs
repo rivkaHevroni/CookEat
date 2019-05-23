63b85c3760391c3094918d5ef34efdf73f6777d1
LianHirsh <48855150+LianHirsh@users.noreply.github.com>
2019-05-23 19:26:12 +0300

    save recipe

---
+++ src/CookEat/Data/Requests/SaveRecipeRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookEat
+{
+	public class SaveRecipeRequest
+	{
+		public string UserId { get; set; }
+		public string RecipeId { get; set; }
+	}
+}
